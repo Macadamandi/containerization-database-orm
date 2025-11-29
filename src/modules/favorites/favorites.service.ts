e1cf3becdd5dfb7603aea0ca548d2026011db9bb
Macadamandi <vlad.yurchenko.2024@gmail.com>
2025-11-29 21:09:02 +0200

    fix: some code fixes

--- src/modules/favorites/favorites.service.ts
+++ src/modules/favorites/favorites.service.ts
@@ -2,11 +2,14 @@ import {
   Injectable,
   BadRequestException,
   NotFoundException,
+  UnprocessableEntityException,
 } from '@nestjs/common';
 import { Favorites, FavoritesResponse } from './interfaces/favorites.interface';
-import { Artist } from '../artist/interfaces/artist.interface';
-import { Album } from '../album/interfaces/album.interface';
-import { Track } from '../track/interfaces/track.interface';
+
+import { ArtistService } from '../artist/artist.service';
+import { AlbumService } from '../album/album.service';
+import { TrackService } from '../track/track.service';
+
 import { validate as isUUID } from 'uuid';
 
 @Injectable()
@@ -14,67 +17,65 @@ export class FavoritesService {
   private favorites: Favorites = { artists: [], albums: [], tracks: [] };
 
   constructor(
-    private artistsData: Artist[] = [],
-    private albumsData: Album[] = [],
-    private tracksData: Track[] = [],
+    private artistService: ArtistService,
+    private albumService: AlbumService,
+    private trackService: TrackService,
   ) {}
 
   getAll(): FavoritesResponse {
     return {
-      artists: this.artistsData.filter((a) =>
-        this.favorites.artists.includes(a.id),
-      ),
-      albums: this.albumsData.filter((a) =>
-        this.favorites.albums.includes(a.id),
-      ),
-      tracks: this.tracksData.filter((t) =>
-        this.favorites.tracks.includes(t.id),
-      ),
+      artists: this.artistService
+        .findAll()
+        .filter((a) => this.favorites.artists.includes(a.id)),
+      albums: this.albumService
+        .findAll()
+        .filter((a) => this.favorites.albums.includes(a.id)),
+      tracks: this.trackService
+        .findAll()
+        .filter((t) => this.favorites.tracks.includes(t.id)),
     };
   }
 
   addTrack(id: string): void {
     if (!isUUID(id)) throw new BadRequestException('Invalid track id');
-    const trackExists = this.tracksData.some((t) => t.id === id);
-    if (!trackExists)
-      throw new NotFoundException(`Track with id ${id} does not exist`);
-    if (!this.favorites.tracks.includes(id)) this.favorites.tracks.push(id);
+    const track = this.trackService.findById(id);
+    if (!track) throw new UnprocessableEntityException('Track does not exist');
+    this.favorites.tracks.push(id);
   }
 
   removeTrack(id: string): void {
     if (!isUUID(id)) throw new BadRequestException('Invalid track id');
     const index = this.favorites.tracks.indexOf(id);
-    if (index === -1) throw new NotFoundException('Track is not in favorites');
+    if (index === -1) throw new NotFoundException('Track not in favorites');
     this.favorites.tracks.splice(index, 1);
   }
 
   addAlbum(id: string): void {
     if (!isUUID(id)) throw new BadRequestException('Invalid album id');
-    const albumExists = this.albumsData.some((a) => a.id === id);
-    if (!albumExists)
-      throw new NotFoundException(`Album with id ${id} does not exist`);
-    if (!this.favorites.albums.includes(id)) this.favorites.albums.push(id);
+    const album = this.albumService.findById(id);
+    if (!album) throw new UnprocessableEntityException('Album does not exist');
+    this.favorites.albums.push(id);
   }
 
   removeAlbum(id: string): void {
     if (!isUUID(id)) throw new BadRequestException('Invalid album id');
     const index = this.favorites.albums.indexOf(id);
-    if (index === -1) throw new NotFoundException('Album is not in favorites');
+    if (index === -1) throw new NotFoundException('Album not in favorites');
     this.favorites.albums.splice(index, 1);
   }
 
   addArtist(id: string): void {
     if (!isUUID(id)) throw new BadRequestException('Invalid artist id');
-    const artistExists = this.artistsData.some((a) => a.id === id);
-    if (!artistExists)
-      throw new NotFoundException(`Artist with id ${id} does not exist`);
-    if (!this.favorites.artists.includes(id)) this.favorites.artists.push(id);
+    const artist = this.artistService.findById(id);
+    if (!artist)
+      throw new UnprocessableEntityException('Artist does not exist');
+    this.favorites.artists.push(id);
   }
 
   removeArtist(id: string): void {
     if (!isUUID(id)) throw new BadRequestException('Invalid artist id');
     const index = this.favorites.artists.indexOf(id);
-    if (index === -1) throw new NotFoundException('Artist is not in favorites');
+    if (index === -1) throw new NotFoundException('Artist not in favorites');
     this.favorites.artists.splice(index, 1);
   }
 }

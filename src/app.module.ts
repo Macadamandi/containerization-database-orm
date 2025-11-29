172ad5b0501fe4af4a2be55fb1684c58cf3f91a3
Macadamandi <vlad.yurchenko.2024@gmail.com>
2025-11-29 20:24:57 +0200

    feat: add album module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './modules/user/user.module';
 import { ArtistModule } from './modules/artist/artist.module';
+import { AlbumModule } from './modules/album/album.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { ArtistModule } from './modules/artist/artist.module';
     }),
     UserModule,
     ArtistModule,
+    AlbumModule,
   ],
 })
 export class AppModule {}
---
+++ src/modules/album/dto/create-album.dto.ts
@@ -0,0 +1,17 @@
+import { IsString, IsUUID, IsInt, Min, Max, IsOptional } from 'class-validator';
+import { Transform } from 'class-transformer';
+
+export class CreateAlbumDto {
+  @Transform(({ value }) => value.trim())
+  @IsString()
+  name: string;
+
+  @IsInt()
+  @Min(1900)
+  @Max(new Date().getFullYear())
+  year: number;
+
+  @IsOptional()
+  @IsUUID()
+  artistId?: string | null;
+}
